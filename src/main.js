817069afe318d5dbbd501d6ea0892f62a804ff82
Hansen0318 <lovehandsomeman@gmail.com>
2026-09-21 16:37:08 +0800

    fix: bust Level7 map rendering cache

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { Game } from './core/Game.js?v=level7-1';
-import { Renderer } from './render/Renderer.js?v=level7-1';
+import { Game } from './core/Game.js?v=level7-mapfix-1';
+import { Renderer } from './render/Renderer.js?v=level7-mapfix-1';
 import { UIController } from './ui/UIController.js?v=level7-1';
 import { ArtStore, LEVEL_REQUIRED_ART_IDS } from './config/artAssets.js?v=level7-1';
 
